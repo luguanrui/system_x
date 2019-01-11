08c2d19fd2f68f62006c6b735b252c98167e144c
luguanrui <1481744045@qq.com>
2019-01-11 22:16:16 +0800

    fix: 关键工艺可视化模拟

--- src/application/visual-simulation/index.vue
+++ src/application/visual-simulation/index.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>可视化模拟</div>
+  <div>
+    <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+      <el-tab-pane label="可视化模拟" name="first">
+        <video controls="controls" width="500" height="500">
+        <source src="http://www.runoob.com/try/demo_source/mov_bbb.mp4">
+        </video>
+      </el-tab-pane>
+      <el-tab-pane label="二维码查看" name="second">
+        <img src="./12.png" alt width="500">
+        <img src="./11.png" alt width="500">
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
 export default {
-
-}
+  data() {
+    return {
+      activeName2: "first"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
 </script>
