c525aa0665c96c0294a9db96b8b80b970d33d9a4
luguanrui <1481744045@qq.com>
2019-01-13 22:59:46 +0800

    perf: 优化

--- src/application/project-profile/index.vue
+++ src/application/project-profile/index.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="project-profile">
     <h4 class="title">工程概况：</h4>
+    <img src="./2.png" alt width="100%">
+    <img src="./3.png" alt width="100%">
     <p class="text">
       演马庄西站是R1线近期终点站，车站位于齐鲁大道上，处于聊城路与青岛路之间，沿齐鲁大道南北向布置，如图3.6所示。演马庄西站为地下两层11m单柱双跨岛式车站，车站总长359.6m，总建筑面积为17063.36m2，标准段总宽19.7m，总高13.5m；盾构井段基坑开挖深度约18.2m，其余段开挖深度为约16.5m。车站设置4个客流出入口，1个安全出口，三组风亭。车站主体结构顶板覆土约2.0～3.0m，底板埋深约16.6～18.1m。
       车站采用明挖法施工，基坑围护分两段，结构一：车站南段172.9m（1-22轴），围护结构采用传统钻孔灌注桩+内支撑（或锚索）结构形式；结构二，车站北段186.7m（22-44轴）采用新工艺施工，为预制方桩复合桩+内支撑结构形式。车站东侧为恒大雅苑高层住宅小区，距离主体结构最近距离35m；车站西侧为规划商务用地，目前尚未建设。
     </p>
-    <img src="./2.png" alt="" width="800">
-    <img src="./3.png" alt="" width="800">
   </div>
 </template>
 <script>
-export default {
-
-}
+export default {};
 </script>
-<style lang="scss">
-.project-profile {
-  width: 800px;
-  margin: 0 auto;
-  .title {
-    font-size: 20px;
-    text-align: left;
-  }
-  .text {
-    margin-bottom: 30px;
-    text-align: justify;
-    line-height: 40px;
-    font-size: 16px;
-  }
-}
-</style>
--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -2,30 +2,30 @@
   <div class="sys-aside">
     <el-menu :router="true" :unique-opened="true">
       <el-submenu index="1">
-        <template slot="title">基本信息</template>
+        <template slot="title"><i class="el-icon-date"></i>基本信息</template>
         <el-menu-item index="project-profile">工程概况</el-menu-item>
       </el-submenu>
       <el-submenu index="2">
-        <template slot="title">BIM 模型管理</template>
+        <template slot="title"><i class="el-icon-upload"></i>BIM 模型管理</template>
         <el-menu-item index="model-iview">模型浏览</el-menu-item>
         <el-menu-item index="model-manage">模型管理</el-menu-item>
       </el-submenu>
       <el-submenu index="3">
-        <template slot="title">进度管理</template>
+        <template slot="title"><i class="el-icon-time"></i> 进度管理</template>
         <el-menu-item index="schedule-query">进度计划查询</el-menu-item>
         <el-menu-item index="schedule-control">进度控制</el-menu-item>
       </el-submenu>
       <el-submenu index="4">
-        <template slot="title">绿色施工</template>
+        <template slot="title"><i class="el-icon-star-on"></i>绿色施工</template>
         <el-menu-item index="implement-desc">绿色施工工艺工法说明</el-menu-item>
         <el-menu-item index="visual-simulation">关键工艺可视化模拟</el-menu-item>
         <el-menu-item index="crash-detection">碰撞检测</el-menu-item>
       </el-submenu>
       <el-menu-item index="quantitative-statistics" class="quantitative-statistics">
-        <span slot="title">工程量统计</span>
+        <span slot="title"><i class="el-icon-search"></i>工程量统计</span>
       </el-menu-item>
       <el-submenu index="6">
-        <template slot="title">安全管理</template>
+        <template slot="title"><i class="el-icon-bell"></i>安全管理</template>
         <el-menu-item index="risk-query">安全风险问题库查询</el-menu-item>
         <el-menu-item index="safety-precaution">安全预警</el-menu-item>
       </el-submenu>
