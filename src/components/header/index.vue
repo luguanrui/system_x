01da4b3e8baa9e66571fe1e179b4a5a5d810a892
luguanrui <1481744045@qq.com>
2019-01-13 22:06:57 +0800

    feat: 退出

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -2,21 +2,24 @@
   <div class="sys-header">
     <h1 class="sys-header--title">
       <i class="logo">
-        <img src="../../assets/timg.png" alt="">
+        <img src="../../assets/timg.png" alt>
       </i>
       济南轨道交通 R1 线演马庄西站绿色施工 BIM 管控平台
     </h1>
+    <div class="logout-btn">
+      <el-button type="text" @click="handleLogout">退出登录</el-button>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {
-
-    }
+    return {};
   },
   methods: {
-
+    handleLogout() {
+      this.$router.push({path: '/login'})
+    }
   }
-}
+};
 </script>
